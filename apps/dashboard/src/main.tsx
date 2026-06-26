0c6523c05aaa5e82c801f269bb88e985675f216a
atmmmmt <salemalnajjar7@gmail.com>
2026-06-26 03:20:17 +0300

    perf: remove DOM translation polling layer

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.js';
 import './styles.css';
-import './domLanguage.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
